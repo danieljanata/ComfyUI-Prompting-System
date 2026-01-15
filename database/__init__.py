6a47d615bb8f28894a380ebdb994216440bca143
danieljanata <daniel.janata.osobni@gmail.com>
2026-01-15 02:10:27 +0100

    Delete database directory

--- database/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Database management for ComfyUI Prompting System"""
-
-from .db_manager import PromptDatabase
-from .image_handler import ImageHandler
-
-__all__ = ['PromptDatabase', 'ImageHandler']
